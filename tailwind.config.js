70e72ecb84fc70ea033042009a149359e20b3371
function07 <houssine199222@gmail.com>
2023-03-10 00:08:47 -0500

    Add HTML with minimal styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: { sideBar: "0.125fr 2fr" },
+      gridTemplateRows: { navBar: "65px" },
+      colors: {
+        orange: {
+          1: "#F3B700",
+          2: "#FAA300",
+          3: "#E57C04",
+          4: "#FF6201",
+          5: "#F63E02",
+        },
+      },
+    },
   },
   plugins: [],
 };

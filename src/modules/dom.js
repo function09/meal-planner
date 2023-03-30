cfd88bc5195272d233cc57626da38f966d656fc4
function07 <houssine199222@gmail.com>
2023-03-30 00:31:51 -0400

    Add class DisplayMealPlan, that will display mealplans from objects

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,14 +1,52 @@
+import { format } from "date-fns";
 import { MealPlanManager } from "./mealplanmanager";
 
+class DisplayMealPlan {
+  constructor(date, breakfast, lunch, dinner) {
+    this.date = date;
+    this.breakfast = breakfast;
+    this.lunch = lunch;
+    this.dinner = dinner;
+  }
+
+  display() {
+    const selectContainer = document.querySelector("#container");
+    const selectCreateNewMealButton = document.querySelector("#createNewMeal");
+
+    const createDiv = document.createElement("div");
+    createDiv.setAttribute("class", "mealPlans");
+
+    const createRemoveButton = document.createElement("button");
+    createRemoveButton.setAttribute("id", "removeMealPlan");
+
+    const createEditButton = document.createElement("button");
+    createEditButton.setAttribute("id", "editMealPlan");
+
+    createDiv.textContent = `${this.date} ${this.breakfast} ${this.lunch} ${this.dinner}`;
+    selectContainer.insertBefore(createDiv, selectCreateNewMealButton);
+
+    createRemoveButton.textContent = "Remove";
+    createDiv.appendChild(createRemoveButton);
+
+    createEditButton.textContent = "Edit";
+    createDiv.appendChild(createEditButton);
+  }
+}
+
 const submitMealPlan = (() => {
-  const newMealPlanManager = new MealPlanManager();
   const selectSubmitFormButton = document.querySelector("#submitFormButton");
-  const date = document.querySelector("#date");
-  const checkboxes = document.querySelectorAll(".checkbox");
-  let mealArray = [];
+  const selectCreateNewMealButton = document.querySelector("#createNewMeal");
+  const selectForm = document.querySelector("#form");
+  const newMealPlanManager = new MealPlanManager();
+
+  selectCreateNewMealButton.addEventListener("click", () => {
+    selectForm.style.display = "flex";
+    selectCreateNewMealButton.style.display = "none";
+  });
 
   selectSubmitFormButton.addEventListener("click", (e) => {
-    e.preventDefault();
+    const mealArray = [];
+    const checkboxes = document.querySelectorAll(".checkbox");
 
     checkboxes.forEach((checkbox) => {
       if (checkbox.checked === true) {
@@ -18,17 +56,25 @@ const submitMealPlan = (() => {
       }
     });
 
-    newMealPlanManager.pushToArray(
-      date.value,
-      mealArray[0],
-      mealArray[1],
-      mealArray[2]
+    const dateValue = document.querySelector("#date").value;
+    const date = format(new Date(dateValue), "PPP");
+    const breakfast = mealArray[0];
+    const lunch = mealArray[1];
+    const dinner = mealArray[2];
+    const newDisplayMealPlan = new DisplayMealPlan(
+      date,
+      breakfast,
+      lunch,
+      dinner
     );
-    console.log(
-      `${date.value} ${mealArray[0]} ${mealArray[1]} ${mealArray[2]}`
-    );
-    mealArray = [];
+
+    newMealPlanManager.pushToArray(date, breakfast, lunch, dinner);
+    newDisplayMealPlan.display();
+    mealArray.length = 0;
+    selectForm.style.display = "none";
+    selectCreateNewMealButton.style.display = "flex";
     console.log(newMealPlanManager.mealPlanArray);
+    e.preventDefault();
   });
 })();
 
